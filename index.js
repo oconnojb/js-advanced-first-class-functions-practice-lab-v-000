8656b73d0ed160bb71d30dbec92961294ff4e77e
John O'Connor <oconnojb@gmail.com>
2018-09-08 23:41:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const logDriversByHometown = function (drivers, loc) {drivers.forEach(function (
   }
 })}
 
-/*
+const driversByRevenue = function (drivers) {
+  let array = []
+  drivers.forEach( function (el, i ar){
+    array.push(el.revenue)
+  })
+  return array
+}
 
-* `logDriversByHometown()` — Receives an array of `driver` objects as the first argument and a location as the second argument.
-  The function logs to the console the `name` attribute of each `driver` whose hometown matches the string passed in as the 'location' argument.
-* `driversByRevenue()` — Receives an array of `driver` objects and returns a new array of `driver` objects sorted by their `revenue` attribute from lowest to highest.
+/*
+* `driversByRevenue()` — Receives an array of `driver` objects and 
+  returns a new array of `driver` objects sorted by their `revenue` attribute from lowest to highest.
 * `driversByName()` — Receives an array of `driver` objects and returns a new array of `driver` objects sorted by their `name` attribute from A to Z. Here, you may have to use the [`String.prototype.localeCompare()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare) method.
 * `totalRevenue()` — Receives an array of `driver` objects and returns the sum of the revenue earned by each driver.
 * `averageRevenue()` — Receives an array of `driver` objects and returns the average revenue earned by each driver.
