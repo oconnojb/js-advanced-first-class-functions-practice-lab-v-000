9e3972f412beae624e0740c3b26b9f6af04b6bdf
John O'Connor <oconnojb@gmail.com>
2018-09-09 00:15:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ const driversByName = function (drivers) {
   a.sort(function(a,b){
     return a.localeCompare(b)
   })
+  return
 }
 
 /*
-* `driversByName()` — Receives an array of `driver` objects 
-  and returns a new array of `driver` objects sorted by their `name` attribute from A to Z. 
+* `driversByName()` — Receives an array of `driver` objects
+  and returns a new array of `driver` objects sorted by their `name` attribute from A to Z.
   Here, you may have to use the [`String.prototype.localeCompare()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare) method.
 * `totalRevenue()` — Receives an array of `driver` objects and returns the sum of the revenue earned by each driver.
 * `averageRevenue()` — Receives an array of `driver` objects and returns the average revenue earned by each driver.
