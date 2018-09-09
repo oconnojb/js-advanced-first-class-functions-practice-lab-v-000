919ec0cb12218548d904a55e31fb98aaa5e47e6b
John O'Connor <oconnojb@gmail.com>
2018-09-09 00:23:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const driversByName = function (drivers) {
 
 const totalRevenue = function (drivers) {
   return drivers.reduce(function(agg, el, i, arr){
+    console.log(el.revenue)
     return agg + el.revenue
   })
 }
