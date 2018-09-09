d78c0b36fa4d8f4b1fac0821414f794185644563
John O'Connor <oconnojb@gmail.com>
2018-09-09 00:15:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,17 @@ const driversByRevenue = function (drivers) {
   return a
 }
 
+const driversByName = function (drivers) {
+  let a = drivers.slice(0, drivers.length)
+  a.sort(function(a,b){
+    return a.localeCompare(b)
+  })
+}
 
 /*
-* `driversByRevenue()` — Receives an array of `driver` objects and
-  returns a new array of `driver` objects sorted by their `revenue` attribute from lowest to highest.
-* `driversByName()` — Receives an array of `driver` objects and returns a new array of `driver` objects sorted by their `name` attribute from A to Z. Here, you may have to use the [`String.prototype.localeCompare()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare) method.
+* `driversByName()` — Receives an array of `driver` objects 
+  and returns a new array of `driver` objects sorted by their `name` attribute from A to Z. 
+  Here, you may have to use the [`String.prototype.localeCompare()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare) method.
 * `totalRevenue()` — Receives an array of `driver` objects and returns the sum of the revenue earned by each driver.
 * `averageRevenue()` — Receives an array of `driver` objects and returns the average revenue earned by each driver.
 */
