8cd43dce73008ac2fb78bfe0c2ee65a00db3b8ba
John O'Connor <oconnojb@gmail.com>
2018-09-09 00:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const logDriversByHometown = function (drivers, loc) {drivers.forEach(function (
 })}
 
 const driversByRevenue = function (drivers) {
-  const d = drivers
+  let d = drivers
   const newarr = d.sort(function(a,b){
     return a.revenue-b.revenue
   })
